64091fae42e6410e6f29d8469036ddd978e99136
Diego Murillo <diegomurilloferrer@gmail.com>
2023-05-16 08:48:04 -0500

    Change database hosting provider

---
+++ social-network-server/Migrations/20230516134349_change-providers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetworkServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeproviders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
